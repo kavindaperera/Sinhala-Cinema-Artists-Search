64e54ede125d98977feb10546d03ea8ea603dd3f
kavindaperera <perera.kavinda52@gmail.com>
2021-10-25 16:55:57 +0530

    Update data cleaning

--- elasticsearch/json_bulk_upload.py
+++ elasticsearch/json_bulk_upload.py
@@ -170,7 +170,7 @@ mapping = {
 
 
 def json_bulk_upload():
-    with open('../artists-corpus/sinhala_artists_data.json') as f:
+    with open('../artists-corpus/clean_sinhala_artists_data.json') as f:
         data = json.loads(f.read())
 
     # stopwords
